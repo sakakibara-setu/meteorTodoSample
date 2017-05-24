febd3f78e300de8f834599fe72f6c34672ef410c
Shohei Ikeda <sakakibara.setu38@gmail.com>
2017-05-25 05:35:34 +0900

    function add task

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,27 @@
 import { Template } from 'meteor/templating';
 
+import { Tasks } from '../api/tasks.js';
+
 import './body.html';
 
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    return Tasks.find({}, { sort: { createAt: -1 } });
+  },
 });
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createAt: new Date(),
+    });
+
+    target.text.value = '';
+  },
+})
